2c19600fb9ddd6dc4fde3d10ba3feca294844754
Arvin <arvin.flores@metalab.csun.edu>
2023-11-18 14:54:09 -0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": [
+    "standard-with-typescript",
+  ],
+  "plugins": [
+    "@stylistic/js"
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+    "@stylistic/js/operator-linebreak": ["error", "after"],
+    "@stylistic/js/max-len": [2, { "code": 120 }],
+    "@stylistic/js/semi": [2, "always"],
+    "@stylistic/js/arrow-parens": [2, "always"],
+    "@stylistic/js/indent": "off",
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      {
+        "multiline": {
+          "delimiter": "semi",
+          "requireLast": true
+        },
+        "singleline": {
+          "delimiter": "semi",
+          "requireLast": true
+        }
+      }
+    ],
+    "@typescript-eslint/strict-boolean-expressions": [
+      2,
+      { "allowNullableBoolean": true }
+    ],
+  }
+};
