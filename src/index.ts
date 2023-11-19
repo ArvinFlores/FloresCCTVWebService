16643aaa820e246c8aa6d5449d847e5e9c7b6afe
Arvin <arvin.flores@metalab.csun.edu>
2023-11-19 12:10:30 -0800

    add api error handler middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
 import { recordingRoutes } from './routes/recording';
+import { apiErrorResponse } from './middleware/api-error-response';
 
 const app = express();
 const port = process.env.PORT ?? 3000;
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use('/api', [
   Router().use('/recordings', recordingRoutes)
 ]);
+app.use(apiErrorResponse);
 
 app.get('/', (_, res) => res.send('FloresCCTV web service'));
 
