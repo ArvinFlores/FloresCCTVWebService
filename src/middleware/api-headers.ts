ebcce1e8f6c31fbc4c5c6fd3bf007b5b0f69ed26
Arvin <arvin.flores@metalab.csun.edu>
2023-11-24 18:39:39 -0800

    update apiHeaders middleware with allow methods

--- src/middleware/api-headers.ts
+++ src/middleware/api-headers.ts
@@ -2,5 +2,6 @@ import type { Request, Response, NextFunction } from 'express';
 
 export function apiHeaders (_: Request, res: Response, next: NextFunction): void {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE');
   next();
 }
